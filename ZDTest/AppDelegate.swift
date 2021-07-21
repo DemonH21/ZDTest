4a8c98e511628897e7b7cc11af8ab2f4e83e97dc
kede Han <hankd_721@foxmail.com>
2021-07-21 16:53:31 +0800

    this 2

--- ZDTest/AppDelegate.swift
+++ ZDTest/AppDelegate.swift
@@ -32,6 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //  这是测试测试测试
     // 呵呵呵这也是测试
     //这是1
+    //这是2
     @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
