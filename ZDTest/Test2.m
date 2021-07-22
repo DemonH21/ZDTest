2d6a80f364f0dc9c20174c35f1191ab33e6f1e37
kede Han <hankd_721@foxmail.com>
2021-07-22 16:18:55 +0800

    this is test2

---
+++ ZDTest/Test2.m
@@ -0,0 +1,20 @@
+//
+//  Test2.m
+//  ZDTest
+//
+//  Created by kede Han on 2021/7/22.
+//
+
+#import "Test2.h"
+
+@implementation Test2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
