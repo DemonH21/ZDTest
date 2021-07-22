413ef5db6d2e03e83b5b097989e52c082d539ca0
kede Han <hankd_721@foxmail.com>
2021-07-22 16:18:55 +0800

    merge

---
+++ ZDTest/Test2.h
@@ -0,0 +1,16 @@
+//
+//  Test2.h
+//  ZDTest
+//
+//  Created by kede Han on 2021/7/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Test2 : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
